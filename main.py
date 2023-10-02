27e7b7bc83636e3279fd692dcddb00694952a4db
RordyC <65141265+RordyC@users.noreply.github.com>
2023-10-02 12:04:15 -0600

    Making sure xml workspace isn't syncing.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 #10/2/2023 I added this line on my laptop in wing!
 #Testing pull on my desktop
 #another line
+#last line for real this time!!
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
