22ad7f175c94230fff3aaee41b13088a5fdd9338
RordyC <rordycrawley@outlook.com>
2023-10-02 11:25:06 -0600

    Added the project to my laptop
    Created a new line in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Rordy
 #Add your names here!!
 
+#10/2/2023 I added this line on my laptop in wing!
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
